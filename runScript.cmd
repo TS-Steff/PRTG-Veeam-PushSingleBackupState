e6db7405e4b0f4de08c25a1549f35b32c649c73a
Stefan Müller - TS-Management GmbH <smueller@ts-management.ch>
2022-03-03 11:59:33 +0100

    Update runScript.cmd
    Added DryRun

--- runScript.cmd
+++ runScript.cmd
@@ -1,3 +1,4 @@
 @echo off
 
-powershell -file "C:\Scripts\veeam_singleJob.ps1" "Job Name"
+powershell -file "C:\Scripts\veeam_singleJob.ps1" "Job Name" -DryRun
+REM powershell -file "C:\Scripts\veeam_singleJob.ps1" "Job Name"
